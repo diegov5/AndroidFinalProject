76051ddec9c309f51733aa42ef14810910ce2f44
diego <diegov0824@hotmail.com>
2019-12-02 14:39:15 -0800

    Changes the image on the navbar to the image of the current playing album

--- app/src/main/java/com/example/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/example/androidfinalproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidfinalproject;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,21 +23,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.ImagesApi;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 
-import com.spotify.protocol.client.Subscription;
-import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.client.CallResult;
+import com.spotify.protocol.types.ImageUri;
+import com.spotify.protocol.types.PlayerContext;
 import com.spotify.protocol.types.Track;
+import com.spotify.protocol.types.Uri;
+import com.squareup.picasso.Picasso;
+
+import java.net.URI;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     final static String TAG = "MainActivity";
-
+    private static final String CLIENT_ID = "e9fa5421cee0490f8c1a636504ceccc8";
+    private static final String REDIRECT_URI = "androidfinalproject://callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+    ImageView naviagtionViewImage;
+    TextView username;
+    NavigationView navigationDrawer;
 
 
     @Override
@@ -44,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        username = findViewById(R.id.username);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        View hview = navigationView.getHeaderView(0);
+        naviagtionViewImage = hview.findViewById(R.id.navigationViewImage);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -85,5 +102,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         Log.d(TAG, "in On New Intent");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        Log.d("MainActivity", "here! Yay!");
+        SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener() {
+            @Override
+            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+
+                mSpotifyAppRemote = spotifyAppRemote;
+                Log.d("MainActivity", "Connected! Yay!");
+
+                // Now you can start interacting with App Remote
+                connected();
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                Log.e("MainActivity", throwable.getMessage(), throwable);
+                Log.e("MainActivity", "Connection failed, error above ^");
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        });
+    }
+
+    private void connected() {
+        // Then we will write some more code here.
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                    }
+                    //PlayerContext currentContext = new PlayerContext();
+                    Log.d(TAG, "Context URI:" + track.imageUri);
+                    //username.setText(mSpotifyAppRemote.getUserApi().toString());
+                    mSpotifyAppRemote.getImagesApi().getImage(track.imageUri)
+                            .setResultCallback(bitmap -> naviagtionViewImage.setImageBitmap(bitmap));
+                    //Picasso.get().load().into(naviagtionViewImage);
+                });
+        
+    }
+
+
+    public void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
 }
--- app/src/main/java/com/example/androidfinalproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/androidfinalproject/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,93 +15,26 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.androidfinalproject.R;
-import com.spotify.android.appremote.api.ConnectionParams;
-import com.spotify.android.appremote.api.Connector;
-import com.spotify.android.appremote.api.SpotifyAppRemote;
-import com.spotify.protocol.client.Subscription;
-import com.spotify.protocol.types.PlayerState;
-import com.spotify.protocol.types.Track;
-import com.spotify.sdk.android.authentication.AuthenticationResponse;
-
-import java.util.Objects;
+import com.example.androidfinalproject.MainActivity;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    private static final String CLIENT_ID = "e9fa5421cee0490f8c1a636504ceccc8";
-    private static final String REDIRECT_URI = "androidfinalproject://callback";
-    private SpotifyAppRemote mSpotifyAppRemote;
-    String authToken;
+    ImageView topAlbum;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        topAlbum = root.findViewById(R.id.topAlbum);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
-        authToken = Objects.requireNonNull(getActivity()).getIntent().getStringExtra("response");
+        //topAlbum.setImageResource();
         return root;
     }
 
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
-
-        ConnectionParams connectionParams =
-                new ConnectionParams.Builder(CLIENT_ID)
-                        .setRedirectUri(REDIRECT_URI)
-                        .showAuthView(true)
-                        .build();
-
-        Log.d("MainActivity", "here! Yay!");
-        SpotifyAppRemote.connect(getContext(), connectionParams, new Connector.ConnectionListener() {
-                    @Override
-                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-
-                        mSpotifyAppRemote = spotifyAppRemote;
-                        Log.d("MainActivity", "Connected! Yay!");
-
-                        // Now you can start interacting with App Remote
-                        connected();
-                    }
-
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        Log.e("MainActivity", throwable.getMessage(), throwable);
-                        Log.e("MainActivity", "Connection failed, error above ^");
-                        // Something went wrong when attempting to connect! Handle errors here
-                    }
-                });
-    }
-
-    private void connected() {
-        // Then we will write some more code here.
-        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
-        mSpotifyAppRemote.getPlayerApi()
-                .subscribeToPlayerState()
-                .setEventCallback(new Subscription.EventCallback<PlayerState>() {
-                    @Override
-                    public void onEvent(PlayerState playerState) {
-                        final Track track = playerState.track;
-                        if (track != null) {
-                            Log.d("MainActivity", track.name + " by " + track.artist.name);
-                        }
-                    }
-                });
-
-
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        // Aaand we will finish off here.
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
-    }
 }
